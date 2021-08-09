22ed4266f6048789a080ecfb2450eaaa1e85742e
mrsekut <k.cloudspider@gmail.com>
2021-08-09 18:39:56 +0900

    feat: initial commit

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{
+  compiler ? "ghc8104",
+}:
+
+let
+  pkgs = import <nixpkgs> { };
+  cabal = pkgs.haskellPackages.cabal-install;
+  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (hpkgs: with hpkgs; [
+  ]);
+
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    cabal
+    ghc
+    stack
+    zlib
+  ];
+}
