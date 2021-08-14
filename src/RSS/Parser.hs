9ff28fa7102d969783eac00f7e863eca69219f1b
mrsekut <k.cloudspider@gmail.com>
2021-08-14 19:02:20 +0900

    feat: convert xml to json

--- src/RSS/Parser.hs
+++ src/RSS/Parser.hs
@@ -1,46 +1,91 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module RSS.Parser where
 
-import Text.XML.HXT.Core
+import           Text.XML.HXT.Core
 
-import System.IO
-import System.Environment
-import System.Console.GetOpt
-import System.Exit
+import qualified Data.Aeson                 as Aeson
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.HashMap.Strict        as HashMap
+import qualified Data.Map                   as M
+import           Data.Maybe
+import qualified Data.Text                  as T
+import           Data.Tree.NTree.TypeDefs
+import qualified Data.Vector                as Vector
+import           System.Console.GetOpt
+import           System.Environment
+import           System.Exit
+import           System.IO
+
 
 main :: IO ()
 main = do
-  [rc]  <- runX $ application [withValidate no] "hoge.xml" "hoge-o.xml"
-  if rc >= c_err
-    then exitWith (ExitFailure (negate 1))
-    else exitSuccess
+  rootElems <- runX $
+    readDocument [] "hoge.xml"
+    >>> getChildren >>> isElem
+  BS.putStr
+    . BS.intercalate (BS.pack "\n")
+    . map (Aeson.encode . wrapRoot . xmlTreeToJSON)
+    $ rootElems
 
 
-application :: SysConfigList -> String -> String -> IOSArrow b Int
-application cfg src dst
-  = configSysVars (withTrace 1 : cfg)
-    >>> traceMsg 1 "start reading document"
-    >>> readDocument [] src
-    >>> traceMsg 1 "document read, start processing"
-    >>> processChildren (selectAllTextAndRealAltValues `when` isElem)
-    >>> traceMsg 1 "document processed"
-    >>> writeDocument [] dst
-    >>> getErrStatus
+
+data Xml
+  = Text
+  | Tag String
+  | Attr String
+  deriving (Eq, Ord, Show)
 
 
-selectAllTextAndRealAltValues :: ArrowXml a => a XmlTree XmlTree
-selectAllTextAndRealAltValues = deep $ isText <+> imageAlt
+wrapRoot :: Maybe (Xml, Aeson.Value) -> Aeson.Value
+wrapRoot Nothing       = Aeson.Null
+wrapRoot (Just (a, b)) = Aeson.object [(packJSValueName a, b)]
+
+
+xmlTreeToJSON :: XmlTree -> Maybe (Xml, Aeson.Value)
+xmlTreeToJSON node@(NTree (XTag qName _) children)
+  = Just (Tag (localPart qName), tagMapToJSValue objMap)
   where
-    imageAlt :: ArrowXml a => a XmlTree XmlTree
-    imageAlt =
-      isElem
-      >>> hasName "image"
-      >>> getAttrValue "href"
-      >>> isA significant
-      >>> arr addBrackets
-      >>> mkText
+    objMap =
+        arrayValuesToJSONArrays    -- unify into a single map,
+      . concatMapValues            -- grouping into arrays by pair name
+      . map (uncurry M.singleton)  -- convert pairs to maps
+      . (++) attrVals
+      . catMaybes                  -- filter out the empty values (unconvertable nodes)
+      $ map xmlTreeToJSON children -- convert xml nodes to Maybe (QName, Aeson.Value) pairs
 
-    significant :: String -> Bool
-    significant = not . all (`elem` " \n\r\t")
+    attrVals =
+      map (Attr *** Aeson.String . T.pack) $ getAttrVals node
 
-    addBrackets :: String -> String
-    addBrackets s = " [[ " ++ s ++ " ]] "
+xmlTreeToJSON (NTree (XText str) _)
+  | T.null text = Nothing
+  | otherwise = Just (Text, Aeson.String text)
+  where
+    text = T.strip $ T.pack str
+
+
+tagMapToJSValue :: M.Map Xml Aeson.Value -> Aeson.Value
+tagMapToJSValue m = case M.toList m of
+  [(Text, val)] -> val
+  _             -> Aeson.Object . HashMap.fromList . (map . first) packJSValueName $ M.toList m
+
+
+arrayValuesToJSONArrays :: M.Map k [Aeson.Value] -> M.Map k Aeson.Value
+arrayValuesToJSONArrays = M.mapMaybe f
+  where
+    f []  = Nothing -- will be discarded
+    f [x] = Just x  -- don't store as array, just a single value
+    f xss = Just $ Aeson.Array . Vector.fromList $ xss -- arrays with more than one element are kept
+
+
+concatMapValues :: (Ord k) => [M.Map k v] -> M.Map k [v]
+concatMapValues = M.unionsWith (++) . (fmap . fmap) (: [])
+
+
+getAttrVals :: XmlTree -> [(String, String)]
+getAttrVals = runLA (getAttrl >>> getName &&& (getChildren >>> getText))
+
+
+packJSValueName :: Xml -> T.Text
+packJSValueName Text     = T.pack "value"
+packJSValueName (Attr x) = T.pack x
+packJSValueName (Tag x)  = T.pack x
